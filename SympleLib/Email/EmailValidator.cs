5f49a17dc21d9edd4211b3d2a382980bb7e486a3
Daniel Lewis <dlewis@sympletech.com>
2012-08-08 09:01:56 -0700

    Changed E-Mail Validator to use http://cobisi.com/email-validation/.net-component

--- SympleLib/Email/EmailValidator.cs
+++ SympleLib/Email/EmailValidator.cs
@@ -1,26 +1,99 @@
-﻿using System.Text.RegularExpressions;
+﻿using Cobisi.EmailVerify;
 
 namespace SympleLib.Email
 {
+    /// <summary>
+    /// Class is simply a wrapper for Cobisi E-Mail Validator 
+    /// (http://cobisi.com/email-validation/.net-component)
+    /// License Is Required to use this componet
+    /// </summary>
     public class EmailValidator
     {
-        public static bool IsValidEmail(string strIn)
+        private EmailVerifier CobisiVerifier;
+
+        public EmailValidator(string CobisiRunTimeKey)
         {
-            return IsValidEmail(strIn, true);
+            EmailVerifier.RuntimeLicenseKey = CobisiRunTimeKey;
+            CobisiVerifier = new EmailVerifier();
         }
+
         /// <summary>
-        /// Checks to ensure a valid e-mail format
+        /// Performs a syntactical validation of the provided
+        /// email address against RFC 1123, RFC 2821, RFC
+        /// 2822, RFC 3696, RFC 4291, RFC 5321 and RFC 5322
+        /// technical specifications, among others.
         /// </summary>
-        public static bool IsValidEmail(string strIn, bool allowBlank)
+        /// <param name="emailAddress"></param>
+        /// <returns></returns>
+        public VerificationResult VaildateEmailSyntax(string emailAddress)
         {
-            if (!string.IsNullOrEmpty(strIn))
-            {
-                // Return true if strIn is in valid e-mail format.
-                return Regex.IsMatch(strIn,
-                       @"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))" +
-                       @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
-            }
-            return allowBlank;
+            return CobisiVerifier.Verify(emailAddress, VerificationLevel.Syntax);
         }
+
+        /// <summary>
+        /// All of the features above, plus:
+        /// Checks the email address domain against a wellknown
+        /// list of disposable email address (DEA)
+        /// providers.
+        /// </summary>
+        /// <param name="emailAddress"></param>
+        /// <returns></returns>
+        public VerificationResult VaildateEmailAgianstKnownDisposableLists(string emailAddress)
+        {
+            return CobisiVerifier.Verify(emailAddress, VerificationLevel.Disposable);
+        }
+
+        /// <summary>
+        /// All of the features above, plus:
+        /// Queries the configured DNS server(s) and looks for
+        /// records about the e-mail address domain and
+        /// checks their validity and consistency.
+        /// </summary>
+        /// <param name="emailAddress"></param>
+        /// <returns></returns>
+        public VerificationResult VaildateEmailUsingDns(string emailAddress)
+        {
+            return CobisiVerifier.Verify(emailAddress, VerificationLevel.Dns);
+        }
+
+        /// <summary>
+        /// All of the features above, plus:
+        /// Checks if a connection could be performed to the
+        /// mail server of the email address domain.
+        /// </summary>
+        /// <param name="emailAddress"></param>
+        /// <returns></returns>
+        public VerificationResult VaildateEmailUsingSmtp(string emailAddress)
+        {
+            return CobisiVerifier.Verify(emailAddress, VerificationLevel.Smtp);
+        }
+
+        /// <summary>
+        /// All of the features above, plus:
+        /// Checks whether the mail server accepts or rejects
+        /// messages sent to the required email address.
+        /// </summary>
+        /// <param name="emailAddress"></param>
+        /// <returns></returns>
+        public VerificationResult VaildateEmailAgianstMailBox(string emailAddress)
+        {
+            return CobisiVerifier.Verify(emailAddress, VerificationLevel.Mailbox);
+        }
+
+        /// <summary>
+        /// All of the features above, plus:
+        /// Checks if the mail server correctly rejects messages
+        /// sent to nonexistent email addresses. This
+        /// verification level allows you to check if a given email
+        /// address is actually a catch-all address.
+        /// -- IN MY TESTING THIS FAILS ALOT --
+        /// </summary>
+        /// <param name="emailAddress"></param>
+        /// <returns></returns>
+        public VerificationResult VaildateEmailIsNotACatchAll(string emailAddress)
+        {
+            return CobisiVerifier.Verify(emailAddress, VerificationLevel.CatchAll);
+        }
+
     }
 }
---
+++ SympleTests/Email/EmailValidatorTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using SympleLib.Email;
+
+namespace SympleTests.Email
+{
+    public class EmailValidatorTests
+    {
+        EmailValidator emailValidator = new EmailValidator("QOwaFKGNGesJrLcoDMj25Pz7c6FYulunhw0XRK7E0oDNANVDie78e1ZdTXsViKSgcf+vfQ==");
+
+        [Test]
+        public void ValidateSyntax()
+        {
+            var result = emailValidator.VaildateEmailSyntax("dlewis@sympletech.com");
+
+            Assert.IsTrue(result.IsSuccess);
+
+        }
+
+        [Test]
+        public void ValidateDisposable()
+        {
+            var result = emailValidator.VaildateEmailAgianstKnownDisposableLists("dlewis@sympletech.com");
+
+            Assert.IsTrue(result.IsSuccess);
+
+        }
+
+        [Test]
+        public void ValidateDNS()
+        {
+            var result = emailValidator.VaildateEmailUsingDns("dlewis@sympletech.com");
+
+            Assert.IsTrue(result.IsSuccess);
+
+        }
+
+        [Test]
+        public void ValidateSMTP()
+        {
+            var result = emailValidator.VaildateEmailUsingSmtp("dlewis@sympletech.com");
+
+            Assert.IsTrue(result.IsSuccess);
+
+        }
+
+        [Test]
+        public void ValidateMailBox()
+        {
+            var result = emailValidator.VaildateEmailAgianstMailBox("dlewis@sympletech.com");
+
+            Assert.IsTrue(result.IsSuccess);
+
+        }
+
+        [Test]
+        public void VaildateEmailIsNotACatchAll()
+        {
+            var result = emailValidator.VaildateEmailIsNotACatchAll("daniel.aaron.lewis@gmail.com");
+            Assert.IsTrue(result.IsSuccess);
+
+        }
+    }
+}
